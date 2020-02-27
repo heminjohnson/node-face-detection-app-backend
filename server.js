5dea2880b817ff5830b0f2164f6378232a02b87b
Hemin Johnson <achayan1992@gmail.com>
2020-02-27 22:19:52 +0100

    feat: add a dummy database

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+
+const database = [
+  {
+    id: "1",
+    name: "Hemin",
+    email: "hemin@gmail.com",
+    password: "secret",
+    entries: 0,
+    joined: new Date()
+  },
+  {
+    id: "2",
+    name: "Johnson",
+    email: "johnson@gmail.com",
+    password: "secret",
+    entries: 0,
+    joined: new Date()
+  }
+];
 
 app.get("/", (req, res) => {
   res.send("This is working");
