c632e7f09f97f6a2d7dac167bb883fb15221862e
Hemin Johnson <achayan1992@gmail.com>
2020-02-27 22:24:26 +0100

    feat: add a dummy sign in endpoint

--- server.js
+++ server.js
@@ -5,27 +5,40 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const database = [
-  {
-    id: "1",
-    name: "Hemin",
-    email: "hemin@gmail.com",
-    password: "secret",
-    entries: 0,
-    joined: new Date()
-  },
-  {
-    id: "2",
-    name: "Johnson",
-    email: "johnson@gmail.com",
-    password: "secret",
-    entries: 0,
-    joined: new Date()
-  }
-];
+const database = {
+  users: [
+    {
+      id: "1",
+      name: "Hemin",
+      email: "hemin@gmail.com",
+      password: "secret",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "2",
+      name: "Johnson",
+      email: "johnson@gmail.com",
+      password: "secret",
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
 
 app.get("/", (req, res) => {
   res.send("This is working");
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error loggin in");
+  }
 });
 
 app.listen(3000, () => {
