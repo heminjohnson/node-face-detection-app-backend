bed81ab206e61754a00431dc18fee154f6fe9f58
Hemin Johnson <achayan1992@gmail.com>
2020-02-27 23:14:33 +0100

    feat: add a dummy register endpoint

--- server.js
+++ server.js
@@ -41,6 +41,21 @@ app.post("/signin", (req, res) => {
   }
 });
 
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+
+  database.users.push({
+    id: "3",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+
+  res.json(database.users[database.users.length - 1]);
+});
+
 app.listen(3000, () => {
   console.log("app is listening on port 3000");
 });
