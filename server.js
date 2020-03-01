f6d747e173b5dedb985ccf385775be008cb92a14
Hemin Johnson <achayan1992@gmail.com>
2020-03-01 16:22:02 +0100

    feat: connect postgres database to server

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const knex = require("knex");
+
+const postgres = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "",
+    password: "",
+    database: "face-detection-app"
+  }
+});
 
 const app = express();
 
