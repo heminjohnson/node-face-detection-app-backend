20cdd4fe939de034873f15242eb961848a423882
Hemin Johnson <achayan1992@gmail.com>
2020-03-06 22:53:53 +0100

    feat: update register endpoint to update the login table as well

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const knex = require("knex");
+const bcrypt = require("bcrypt-nodejs");
 
 const db = knex({
   client: "pg",
@@ -61,17 +62,31 @@ app.post("/register", (req, res) => {
     return res.status(400).json("unable to join");
   }
 
-  db("users")
-    .returning("*")
-    .insert({
-      name: name,
-      email: email,
-      joined: new Date()
-    })
-    .then(user => {
-      res.json(user[0]);
-    })
-    .catch(err => res.status(400).json("unable to join"));
+  const hash = bcrypt.hashSync(password);
+
+  db.transaction(trx => {
+    trx
+      .insert({
+        hash: hash,
+        email: email
+      })
+      .into("login")
+      .returning("email")
+      .then(loginEmail => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            name: name,
+            email: loginEmail,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+          .then(trx.commit)
+          .catch(trx.rollback);
+      });
+  }).catch(err => res.status(400).json("unable to join"));
 });
 
 app.get("/profile/:id", (req, res) => {
