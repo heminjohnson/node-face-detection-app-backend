1b07a9fbbc7a6b134572afb60f3251d2b0a1982a
Hemin Johnson <achayan1992@gmail.com>
2020-03-07 01:13:05 +0100

    feat: update the signin endpoint and connect it to the database

--- server.js
+++ server.js
@@ -45,14 +45,26 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json("success");
-  } else {
-    res.status(400).json("error loggin in");
-  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      console.log(isValid)
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch(err => res.status(400).json("Wrong credentials"));
 });
 
 app.post("/register", (req, res) => {
@@ -77,7 +89,7 @@ app.post("/register", (req, res) => {
           .returning("*")
           .insert({
             name: name,
-            email: loginEmail,
+            email: loginEmail[0],
             joined: new Date()
           })
           .then(user => {
